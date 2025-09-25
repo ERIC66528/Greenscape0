94a27d8986f14702291ee027d5f4f015ff783235
Eric <mateieric66@gmail.com>
2025-09-25 06:33:58 +0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,6 @@
+if(location.pathname.endsWith('login.html')||location.pathname.endsWith('/')){
+}
+if(location.pathname.endsWith('login.html')){
+}
+if(location.pathname.endsWith('register.html')){
+}
